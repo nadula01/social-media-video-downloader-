45343eefb550ff325471d9be63a50c7cdad2eafa
nadula01 <nadulachandraprabha63@gmail.com>
2026-06-22 20:28:35 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -50,12 +50,15 @@ def download_video():
 
     
     if is_audio:
-       ydl_opts = {
+      ydl_opts = {
     'format': 'best',
+    'extractor_args': {'youtube': {'player_client': ['android', 'web']}},
+    'nocheckcertificate': True,
+    'geo_bypass': True,
     'http_headers': {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
+    }
+}
     }
 }
     else:
